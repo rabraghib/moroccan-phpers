aff2eccb8b1028f7d236f7ab9993d23bb41503fe
Rabyâ Raghib <rabraghib@gmail.com>
2022-02-23 22:24:29 +0100

    feat: add navbar & setup Ngaox-Icons

---
+++ src/app/app.icons.ts
@@ -0,0 +1,22 @@
+import { INgaoxIcon } from "@ngaox/icons";
+
+export const APP_ICONS: INgaoxIcon[] = [
+  {
+    name: "moroccan-phpers",
+    data: `
+      <svg viewBox="0 0 52 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.87695 53H28.7791C41.5357 53 51.877 42.7025 51.877 30H24.9748C12.2182 30 1.87695 40.2975 1.87695 53Z" fill="#76A9FA" />
+        <path d="M0.000409561 32.1646L0.000409561 66.4111C12.8618 66.4111 23.2881 55.9849 23.2881 43.1235L23.2881 8.87689C10.9966 8.98066 1.39567 19.5573 0.000409561 32.1646Z" fill="#A4CAFE" />
+        <path d="M50.877 5H23.9748C11.2182 5 0.876953 15.2975 0.876953 28H27.7791C40.5357 28 50.877 17.7025 50.877 5Z" fill="#1C64F2" />
+      </svg>
+    `,
+  },
+  {
+    name: "menu-bars",
+    data: `
+      <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+      </svg>
+    `,
+  },
+];
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
+})
+export class NavbarComponent implements OnInit {
+  isMenuOpened = false;
+  links = [
+    {
+      name: "Challenges",
+      url: "/challenges",
+    },
+    {
+      name: "Submissions",
+      url: "/submissions",
+    },
+    {
+      name: "Leaderboard",
+      url: "/leaderboard",
+    },
+    {
+      name: "About",
+      url: "/about",
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleMenu() {
+    this.isMenuOpened = !this.isMenuOpened;
+  }
+}
